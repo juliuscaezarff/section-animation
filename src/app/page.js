c57d301194c7b8c41427467bccd8e9fa0fbd9fbd
julius caezar <julius.caezar25@gmail.com>
2024-05-23 23:29:58 -0400

    feat: creating preload animation and import Projects

--- src/app/page.js
+++ src/app/page.js
@@ -1,49 +1,29 @@
 'use client'
-
-import { useState } from 'react'
 import styles from './page.module.css'
-import Project from '../components/project'
+import { useEffect, useState } from 'react'
+import { AnimatePresence } from 'framer-motion'
+import Preloader from '../components/Preloader'
+import Projects from '../components/Projects'
 
 export default function Home() {
-  const projects = [
-    {
-      title: 'C2 Montreal',
-      src: 'c2montreal.png',
-      color: '#000000'
-    },
-    {
-      title: 'Office Studio',
-      src: 'officestudio.png',
-      color: '#8C8C8C'
-    },
-    {
-      title: 'Locomotive',
-      src: 'locomotive.png',
-      color: '#EFE8D3'
-    },
-    {
-      title: 'Silencio',
-      src: 'silencio.png',
-      color: '#706D63'
-    }
-  ]
+  const [isLoading, setIsLoading] = useState(true)
 
-  const [modal, setModal] = useState({ active: false, index: 0 })
+  useEffect(() => {
+    (async () => {
+      setTimeout(() => {
+        setIsLoading(false)
+        document.body.style.cursor = 'default'
+        window.scrollTo(0, 0)
+      }, 2000)
+    })()
+  }, [])
 
   return (
     <main className={styles.main}>
-      <div className={styles.body}>
-        {projects.map((project, index) => {
-          return (
-            <Project
-              key={index}
-              index={index}
-              title={project.title}
-              setModal={setModal}
-            />
-          )
-        })}
-      </div>
+      <AnimatePresence mode="wait">
+        {isLoading && <Preloader />}
+      </AnimatePresence>
+      <Projects />
     </main>
   )
 }
