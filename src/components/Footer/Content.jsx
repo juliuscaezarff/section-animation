8e76669f6179981c33a3eb82b2c174c643b583e2
julius caezar <julius.caezar25@gmail.com>
2024-06-09 16:43:27 -0400

    feat: estilização

--- src/components/Footer/Content.jsx
+++ src/components/Footer/Content.jsx
@@ -1,9 +1,10 @@
 import Link from 'next/link'
 import React from 'react'
+import { ArrowTopRightIcon } from '@radix-ui/react-icons'
 
 export default function Content() {
   return (
-    <div className='bg-[#4E4E5A] py-8 px-12 h-full w-full flex flex-col justify-between'>
+    <div className='bg-[#141516] py-8 px-12 h-full w-full flex flex-col justify-between'>
         <Section1 />
         <Section2 />
     </div>
@@ -21,7 +22,7 @@ const Section1 = () => {
 const Section2 = () => {
   return (
     <div className="flex justify-between items-end text-[#CECCD7]">
-      <h1 className="text-[8vw] leading-[0.8] mt-10">Vamos Juntos Criar</h1>
+      <Link href={"/"} className="text-[8.04vw] leading-[0.8] mt-10">Vamos Juntos Criar</Link>
       <p>©copyright</p>
     </div>
   )
@@ -31,14 +32,16 @@ const Nav = () => {
   return (
     <div className="flex shrink-0 gap-20 text-[#CECCD7]">
       <div className="flex flex-col gap-2">
+      <h3 className='mb-2 uppercase text-[#ffffff80]'>Navegação</h3>
         <p>Início</p>
         <p>Projetos</p>
         <p>Sobre</p>
         <p>Contato</p>
       </div>
       <div className="flex flex-col gap-2">
-        <Link href={"https://www.linkedin.com/in/julius-caezar/"} target="_blank">Whatsaap</Link>
-        <p>Linkedin</p>
+      <h3 className='mb-2 uppercase text-[#ffffff80]'>Links</h3>
+        <Link className='flex items-center gap-1 font-General font-semibold text-xl' href={"https://www.linkedin.com/in/julius-caezar/"} target="_blank"><ArrowTopRightIcon /> Whatsaap</Link>
+        <Link className='flex items-center gap-1 font-General font-semibold text-xl' href={"/"}><ArrowTopRightIcon /> Linkedin</Link>
       </div>
     </div>
   )
