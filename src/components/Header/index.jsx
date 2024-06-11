2aa2ef2a8689b59cc08b9faedc20d7daafd1a01f
Julius Caezar <julius.caezar25@gmail.com>
2024-06-11 18:56:07 -0300

    feat: removendo um link

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,11 +5,12 @@ import { usePathname } from 'next/navigation'
 import { NavLink } from './nav-items'
 import { HamburgerMenuIcon } from '@radix-ui/react-icons'
 import { Drawer, DrawerContent, DrawerTrigger } from '../ui/drawer'
+import Link from 'next/link'
 
 export function Header() {
   const pathname = usePathname()
 
-  const isActive = (path) => {
+  const isActive = path => {
     return pathname === path
   }
 
@@ -17,16 +18,13 @@ export function Header() {
     <header className="">
       <div className="p-2 px-4  mx-auto w-full max-w-[1885px] flex items-center justify-between">
         {/* <Image src="/images/point.svg" alt="logo" width={38} height={38} /> */}
-        <h1 className='text-base font-general font-semibold '>Julius.Caezar</h1>
-        
+        <Link href={"/"} className="text-base font-general font-semibold ">Julius.Caezar</Link>
+
         <nav className="hidden md:flex items-center gap-4">
-          <NavLink href="/app" active={isActive('/app')}>
-            Projetos
-          </NavLink>
-          <NavLink href="/projects" active={isActive('/projects')}>
+          <NavLink href="/sobre" active={isActive('/sobre')}>
             Sobre
           </NavLink>
-          <NavLink href="/projects" active={isActive('/projects')}>
+          <NavLink href="/contato" active={isActive('/contato')}>
             Contato
           </NavLink>
         </nav>
@@ -34,15 +32,15 @@ export function Header() {
         <div className="md:hidden">
           <Drawer>
             <DrawerTrigger>
-              <HamburgerMenuIcon className='w-[38px] h-[38px]'/>
+              <HamburgerMenuIcon className="w-[38px] h-[38px]" />
             </DrawerTrigger>
             <DrawerContent>
               <nav className="flex flex-col items-center gap-4">
-                <NavLink href="/app" active={isActive('/app')}>
-                  Blog
+                <NavLink href="/sobre" active={isActive('/sobre')}>
+                  Sobre
                 </NavLink>
-                <NavLink href="/projects" active={isActive('/projects')}>
-                  Projects
+                <NavLink href="/contato" active={isActive('/contato')}>
+                  Contato
                 </NavLink>
               </nav>
             </DrawerContent>
